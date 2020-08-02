907aaae1e7ec0c333e66a69724315f847abeee11
Ross Khapilov <rossxpanteley@gmail.com>
2020-08-02 19:38:16 +0300

    -migrate from Mustache to Freemarker Template

--- src/main/java/com/khapilov/currency/converter/controllers/RegistrationController.java
+++ src/main/java/com/khapilov/currency/converter/controllers/RegistrationController.java
@@ -5,11 +5,11 @@ import com.khapilov.currency.converter.domain.User;
 import com.khapilov.currency.converter.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collections;
-import java.util.Map;
 
 /**
  * @author Ross Khapilov
@@ -21,17 +21,23 @@ public class RegistrationController {
     private UserRepo userRepo;
 
     @GetMapping("/registration")
-    public String registration() {
+    public String registration(Model model) {
+        model.addAttribute("message", "Введите логин и пароль");
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Map<String, Object> model) {
+    public String addUser(User user, Model model) {
         User userFromDb = userRepo.findByUsername(user.getUsername());
         if (userFromDb != null) {
-            model.put("message", "Пользователь с таким именем уже зарегистрирован");
+            model.addAttribute("message", "Пользователь с таким именем уже зарегистрирован");
             return "registration";
         }
+        if (user.getPassword() == null || user.getPassword().isEmpty()) {
+            model.addAttribute("message", "Пароль не может быть пустым");
+            return "registration";
+        }
+        model.addAttribute("message", "Введите логин и пароль");
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
         userRepo.save(user);
